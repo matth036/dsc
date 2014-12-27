1a8047e6a88e435a5e3ef74bc4886ec2c9310e8a
Dan Matthews <matth036@umn.edu>
2014-12-27 13:24:08 -0600

    Fixed test/Makefile to compile starlist_access.cpp

--- Makefile
+++ Makefile
@@ -100,6 +100,7 @@ CPPSRC+=quadrature_decoder.cpp
 CPPSRC+=sexagesimal.cpp
 CPPSRC+=navigation_star.cpp
 CPPSRC+=starlist_access.cpp
+CPPSRC+=ngc_list_access.cpp
 CPPSRC+=controller.cpp
 CPPSRC+=telescope_model.cpp
 CPPSRC+=rtc_management.cpp
--- starlist_access.cpp
+++ starlist_access.cpp
@@ -37,11 +37,7 @@ CAA2DCoordinate starlist_access::proper_motion_adjusted_position( int index, dou
   double DECrad =
       CAACoordinateTransformation::DegreesToRadians(DEC.to_double());
   double cos_factor = cos(DECrad);
-#if 0
-  CAA2DCoordinate pm_arc_sec;
-  pm_arc_sec.X = starlist[index].RApm * years / 3600;
-  pm_arc_sec.Y = starlist[index].DECpm * years / 3600;
-#endif
+
   if (cos_factor != 0.0) { /* This approach fails at the poles.  */
     ra_dec.X += starlist[index].RApm * years * (24.0 / 360.0) * (1.0 / 3600.0) / cos_factor;
     ra_dec.Y += starlist[index].DECpm * years * (1.0 / 3600.0);
