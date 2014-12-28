4e9dc2b38b06ce3e5db7392ed8235727d28177a8
Dan Matthews <matth036@umn.edu>
2014-12-28 08:40:36 -0600

    properly added refraction_temperature_pressure.h
    No refraction_temperature_pressure.cpp yet.

---
+++ refraction_temperature_pressure.h
@@ -0,0 +1,8 @@
+#ifndef  _REFRACTION_TEMPERATURE_PRESSURE_H 
+#define  _REFRACTION_TEMPERATURE_PRESSURE_H
+
+// const float DEFAULT_PRESSURE = 1010.0;   /* mb (millibars) */
+const float DEFAULT_PRESSURE = 0.0;   /* mb (millibars) */
+const float DEFAULT_TEMPERATURE = 10.0;  /* Celsius        */
+
+#endif  /*  _REFRACTION_TEMPERATURE_PRESSURE_H  */
