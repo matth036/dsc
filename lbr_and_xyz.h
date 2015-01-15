f7879332971d5d37e09bdfb56c6ca827f5d92ed6
Dan Matthews <matth036@yahoo.com>
2015-01-15 14:44:21 -0600

    add files omitted earlier.

---
+++ lbr_and_xyz.h
@@ -0,0 +1,12 @@
+#ifndef _LBR_AND_XYZ_H
+#define _LBR_AND_XYZ_H
+
+#include "AA3DCoordinate.h"
+
+CAA3DCoordinate LBR_to_XYZ(CAA3DCoordinate lbr);
+/* Reverse transformation. */
+CAA3DCoordinate XYZ_to_LBR(CAA3DCoordinate xyz);
+
+CAA3DCoordinate AziAltR_to_XYZ(CAA3DCoordinate azi_alt_r);
+CAA3DCoordinate XYZ_to_AziAltR(CAA3DCoordinate xyz);
+#endif   /*   _LBR_AND_XYZ_H  */
