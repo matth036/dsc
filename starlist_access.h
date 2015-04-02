27acdb26c3f0a71ad974939cc25d8e752c49cd70
Dan Matthews <matth036@yahoo.com>
2015-04-02 17:22:01 -0500

    Extensive comment added to starlist_access.h.
    Also removed some garbage files.

--- starlist_access.h
+++ starlist_access.h
@@ -1,6 +1,35 @@
 #ifndef _STARLIST_ACCESS_H
 #define _STARLIST_ACCESS_H
 
+/*
+ * Problem: Avoid double inclusion of large aggregate definitions.
+ *
+ * Standard Solution: Do not put definitions of aggregate objects in 
+ * header files. Rather put them in .cpp or .c files so the linker 
+ * can sort out the duplication.  See for example Stroustrup 4th 
+ * edition 15.2.2.
+ *
+ * NOT USING THE STANDARD SOLUTION because the large aggregate is 
+ * machine generated. (And because in the future the list may become
+ * a database proper.)
+ *
+ * Solution for this project:
+ * Large aggregate   
+ * static const star_data starlist[] = { {...}, ... }; 
+ * is defined in machine generated file starlist.h.
+ * 
+ * starlist.h is NOT #included in this file.
+ * starlist.h is ONLY #included in starlist_access.cpp
+ * 
+ * Other source files needing the starlist[] information
+ * #include "starlist_access.h" (This file) and use
+ * the accessor methods declared here and
+ * defined in starlist_access.cpp.
+ *
+ * Plan to do the same for New General Catalog objects.
+ *
+ */
+
 #include "AA2DCoordinate.h"
 #include <inttypes.h>
 
