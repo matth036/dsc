88a1972c02ce9934596730bda29206307a36bac4
Dan Matthews <matth036@yahoo.com>
2015-04-29 19:16:48 -0500

    Truthified some comments and removed dead code.

--- specificities.cpp
+++ specificities.cpp
@@ -3,7 +3,7 @@
 #include "specificities.h"
 
 /* MACROS such as GPIO_Pin_7 are #defined in stm32f4xx_gpio.h */
-/* MACROS such as GPIOD are #defined in stm32f4xx_gpio.h      */
+
 
 std::initializer_list<uint16_t> specificities::char_lcd_pins
 {
@@ -20,6 +20,8 @@ std::initializer_list<uint16_t> specificities::char_lcd_pins
     GPIO_Pin_15,       /* DB7 */
 };
 
+/* MACROS such as GPIOD are #defined in stm32f4xx_gpio.h                */
+/* GPIO_TypeDef* is a pointer to a struct typedef found in stm32f4xx.h  */
 std::initializer_list<GPIO_TypeDef*>  specificities::char_lcd_ports
 {
   GPIOD,         /* RS  */
--- specificities.h
+++ specificities.h
@@ -10,6 +10,9 @@
  * should be end user set-able in a finished product. 
  */
 namespace specificities{
+  extern std::initializer_list<uint16_t>       char_lcd_pins;
+  extern std::initializer_list<GPIO_TypeDef*>  char_lcd_ports;
+
   constexpr uint32_t altitude_ticks_per_revolution = 4000;
   constexpr uint32_t azimuth_ticks_per_revolution = 4000;
 
@@ -28,46 +31,6 @@ namespace specificities{
   constexpr uint32_t my_latitude = 45;
   constexpr uint32_t altitude_startup_count = (altitude_ticks_per_revolution*my_latitude)/360;
   constexpr uint32_t azimuth_startup_count = 0;
-
-  /* MACROS such as GPIO_Pin_7 are #defined in stm32f4xx_gpio.h */
-
-
-  constexpr uint16_t char_lcd_RS_pin  = GPIO_Pin_0;        /* RS  */
-  constexpr uint16_t char_lcd_RW_pin  = GPIO_Pin_2;        /* R/W */
-  constexpr uint16_t char_lcd_E_pin   = GPIO_Pin_4;        /* E  (Enable) */
-  constexpr uint16_t char_lcd_DB0_pin = GPIO_Pin_6;        /* DB0 */
-  constexpr uint16_t char_lcd_DB1_pin = GPIO_Pin_8;        /* DB1 */
-  constexpr uint16_t char_lcd_DB2_pin = GPIO_Pin_10;       /* DB2 */
-  constexpr uint16_t char_lcd_DB3_pin = GPIO_Pin_14;       /* DB3 */
-  constexpr uint16_t char_lcd_DB4_pin = GPIO_Pin_7;        /* DB4 */
-  constexpr uint16_t char_lcd_DB5_pin = GPIO_Pin_9;        /* DB5 */
-  constexpr uint16_t char_lcd_DB6_pin = GPIO_Pin_11;       /* DB6 */
-  constexpr uint16_t char_lcd_DB7_pin = GPIO_Pin_15;       /* DB7 */
-
-  // 
-  extern std::initializer_list<uint16_t>       char_lcd_pins;
-  extern std::initializer_list<GPIO_TypeDef*>  char_lcd_ports;
-
-
-  // A pointer to a struct. 
-  // using port = GPIO_TypeDef*; 
-
-  constexpr GPIO_TypeDef* char_lcd_RS_port  = GPIOD;        /* RS  */
-  constexpr GPIO_TypeDef* char_lcd_RW_port  = GPIOD;        /* R/W */
-  constexpr GPIO_TypeDef* char_lcd_E_port   = GPIOD;        /* E  (Enable) */
-  constexpr GPIO_TypeDef* char_lcd_DB0_port = GPIOD;        /* DB0 */
-  constexpr GPIO_TypeDef* char_lcd_DB1_port = GPIOD;        /* DB1 */
-  constexpr GPIO_TypeDef* char_lcd_DB2_port = GPIOD;        /* DB2 */
-  constexpr GPIO_TypeDef* char_lcd_DB3_port = GPIOD;        /* DB3 */
-  constexpr GPIO_TypeDef* char_lcd_DB4_port = GPIOD;        /* DB4 */
-  constexpr GPIO_TypeDef* char_lcd_DB5_port = GPIOD;        /* DB5 */
-  constexpr GPIO_TypeDef* char_lcd_DB6_port = GPIOD;        /* DB6 */
-  constexpr GPIO_TypeDef* char_lcd_DB7_port = GPIOD;        /* DB7 */
-
-
-
-
-
 }
 
 #endif   /*  _SPECIFICITIES_H  */
