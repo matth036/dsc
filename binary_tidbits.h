6c5912e8351d7d0bc7e29b0ba28a393460e97a1d
Dan Matthews <matth036@yahoo.com>
2015-10-17 13:20:15 -0500

    de-linting

--- binary_tidbits.h
+++ binary_tidbits.h
@@ -1,24 +1,25 @@
-#ifndef _BINARY_TIDBITS_H
-#define _BINARY_TIDBITS_H
+#ifndef BINARY_TIDBITS_H_
+#define BINARY_TIDBITS_H_
 
-/* These values could well be contexpr. We provide them as functions
+/*! 
+ * These values could well be contexpr. We provide them as functions
  * as a reminder that these are only conventions.  Reversing any of 
  * these should have no visible effect after a complete rebuild.  
  * But don't expect that I have tested this.
  */
-namespace binary_tidbits{
+namespace binary_tidbits {
 
   /*
    * See Meeuse, Astronimical Algorithims 2nd Edition pp. 93.
    */
-  inline bool west_longitude_is_positive(){
+  inline bool west_longitude_is_positive() {
     return true;
   }
 
-  /* Adopt the conventions of a northern hemisphere navigator.
-   *
+  /* 
+   * Adopt the conventions of a northern hemisphere navigator.
    */
-  inline bool zero_azimuth_is_north(){
+  inline bool zero_azimuth_is_north() {
     return true;
   }
 
@@ -28,8 +29,6 @@ namespace binary_tidbits{
   }
   *******************************/
 
+}  // namespace binary_tidbits
 
-
-}
-
-#endif /* _BINARY_TIDBITS_H */
+#endif  // BINARY_TIDBITS_H_
